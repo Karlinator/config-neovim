897019ab258f8dc24877ebb81273f17ccf36c9be
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-20 16:10:15 +0200

    Add Neotree

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd('set expandtab')
 vim.cmd('set tabstop=4')
 vim.cmd('set softtabstop=2')
 vim.cmd('set shiftwidth=2')
+vim.cmd('set nu rnu')
 
 require("config.lazy")
 vim.cmd.colorscheme "catppuccin"
@@ -13,5 +14,8 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
+
 
 
