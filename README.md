38f7668df094f3518dc2e9a2444472a88420b616
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-21 00:53:17 +0200

    Add debugging (with gdb)

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Neovim Config
+
+## External requirements
+
+This will mostly work so long as a recent Neovim is installed, but there are a couple of things that only work with external packages.
+
+DNF command because I use Fedora:
+
+```
+dnf install neovim python golang lua luarocks lua5.1
+```
---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "-i", "dap" },
+      }
+      dap.configurations.c = {
+        {
+          name = "Launch",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtBeginningOfMainSubprogram = false,
+        },
+      }
+
+      vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+}
