2a7107334033769f7244ad39cc8a69f059833261
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-21 00:02:53 +0200

    Add completions

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,24 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+			}),
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+		})
+	end,
+}
