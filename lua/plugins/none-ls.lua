9c3e0e20c53ce73689654e8f7f89b10e123a91a1
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-20 23:28:56 +0200

    Add none-ls-ext

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
@@ -12,6 +15,7 @@ return {
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.rpmspec,
         null_ls.builtins.formatting.prettierd,
+        require("none-ls.diagnostics.eslint_d"),
       },
     })
 
