bbb8810a3bee30972a043e67ead436e97ddb78ca
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-20 23:18:00 +0200

    More code actions for formatting/linting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,20 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.code_actions.gitrebase,
+        null_ls.builtins.diagnostics.checkmake,
+        null_ls.builtins.diagnostics.codespell,
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.rpmspec,
+        null_ls.builtins.formatting.prettierd,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
