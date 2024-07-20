3ec088983e29f645bd73daf4612aa3ffd1e14419
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-20 21:33:04 +0200

    Move config to plugins config

--- init.lua
+++ init.lua
@@ -1,21 +1,8 @@
-
-vim.cmd('set expandtab')
-vim.cmd('set tabstop=4')
-vim.cmd('set softtabstop=2')
-vim.cmd('set shiftwidth=2')
-vim.cmd('set nu rnu')
-
 require("config.lazy")
-vim.cmd.colorscheme "catppuccin"
+require("config.vim")
 
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
 
 
 
