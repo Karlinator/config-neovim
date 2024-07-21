3ad8486e743ff13889b32541dbafd53b71161b43
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-21 14:08:48 +0200

    Only format with none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,47 +1,48 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {},
-      })
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {},
+			})
 
-      vim.keymap.set("n", "<leader>gf", function()
-        vim.lsp.buf.format({
-          filter = function(client)
-            return client.name ~= "tsserver"
-          end,
-        })
-      end, {})
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    config = function()
-      require("mason-null-ls").setup({
-        ensure_installed = {
-          "prettierd",
-          "eslint_d",
-          "stylua",
-          "gitsigns",
-          "gitrebase",
-          "codespell",
-          "mypy",
-          "rpmspec",
-          "chackmake",
-        },
-        automatic_installation = false,
-        handlers = {},
-      })
-    end,
-  },
+			vim.keymap.set("n", "<leader>gf", function()
+				vim.lsp.buf.format({
+					filter = function(client)
+						return client.name == "null-ls"
+					end,
+				})
+			end, {})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"prettierd",
+					"eslint_d",
+					"stylua",
+					"gitsigns",
+					"gitrebase",
+					"codespell",
+					"mypy",
+					"rpmspec",
+					"chackmake",
+				},
+				automatic_installation = false,
+				handlers = {},
+			})
+		end,
+	},
 }
