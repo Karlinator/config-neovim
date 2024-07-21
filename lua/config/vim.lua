34ad4e4b78fce9d1489954c60a118116062d81cc
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-21 15:49:04 +0200

    Better keybinds for terminal mode

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -3,13 +3,15 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nu rnu")
-vim.opt.smartindent=false
+vim.opt.smartindent = false
+
+vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { silent = true })
 
 vim.api.nvim_create_user_command("StupidIndent", function()
-    vim.cmd("set noexpandtab tabstop=8 shiftwidth=4 softtabstop=4")
-    vim.cmd("TSDisable indent")
+	vim.cmd("set noexpandtab tabstop=8 shiftwidth=4 softtabstop=4")
+	vim.cmd("TSDisable indent")
 end, {})
 vim.api.nvim_create_user_command("NormalIndent", function()
-    vim.cmd("set expandtab tabstop=4 shiftwidth=4 softtabstop=4")
-    vim.cmd("TSEnable indent")
+	vim.cmd("set expandtab tabstop=4 shiftwidth=4 softtabstop=4")
+	vim.cmd("TSEnable indent")
 end, {})
