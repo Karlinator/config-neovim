c7160e5ce1ae4b6a8854236990dd46bf9c13f762
Karl Erik Hofseth <karl@kehofseth.no>
2024-07-21 12:34:09 +0200

    Add mason-null-ls to manage null-ls sources

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,41 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.code_actions.gitrebase,
-        null_ls.builtins.diagnostics.checkmake,
-        null_ls.builtins.diagnostics.codespell,
-        null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.rpmspec,
-        null_ls.builtins.formatting.prettierd,
-        require("none-ls.diagnostics.eslint_d"),
-      },
-    })
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {},
+      })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          "prettierd",
+          "eslint_d",
+          "stylua",
+          "gitsigns",
+          "gitrebase",
+          "codespell",
+          "mypy",
+          "rpmspec",
+          "chackmake",
+        },
+        automatic_installation = false,
+        handlers = {},
+      })
+    end,
+  },
 }
