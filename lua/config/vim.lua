17f569b679d91668d7362bcb9e5a930e59fe6c12
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2024-09-19 11:23:31 +0200

    Automatically set StupidIndent based on working directory

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -8,13 +8,21 @@ vim.opt.smartindent = false
 vim.keymap.set("t", "<esc><leader>", "<C-\\><C-n>", { silent = true })
 
 vim.api.nvim_create_user_command("StupidIndent", function(opts)
-    local indent = opts.fargs[1] or "2"
+	local indent = opts.fargs[1] or "2"
 	vim.cmd(string.format("set noexpandtab tabstop=8 shiftwidth=%s softtabstop=%s", indent, indent))
 	vim.cmd("TSDisable indent")
-end, { nargs='?' })
+end, { nargs = "?" })
 vim.api.nvim_create_user_command("NormalIndent", function()
 	vim.cmd("set expandtab tabstop=4 shiftwidth=4 softtabstop=4")
 	vim.cmd("TSEnable indent")
 end, {})
+
+local stupidIndents = { OpointLuceneLib = 4, opointlucenelib=4, SolrIndex = 4, ["search-system"] = 2, ["search-system-karl"] = 2 }
+
+local path = vim.fn.getcwd():match("([^/]+)$")
+
+if stupidIndents[path] then
+	vim.cmd(string.format("StupidIndent %s", stupidIndents[path]))
+end
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
