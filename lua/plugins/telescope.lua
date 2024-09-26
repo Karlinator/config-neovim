1ec51ad63a9197df6a0b16f2e522028315531a8c
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2024-09-26 15:55:05 +0200

    Configure telescope to search dotfiles while still respecting gitignore

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,17 @@ return {
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
-		config = function()
+		opts = {
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+				},
+				live_grep = {
+					additional_args = { "--iglob", "!.git", "--hidden" },
+				},
+			},
+		},
+		init = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
@@ -14,14 +24,14 @@ return {
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
+		opts = {
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
 				},
-			})
+			},
+		},
+		init = function()
 			require("telescope").load_extension("ui-select")
 		end,
 	},
