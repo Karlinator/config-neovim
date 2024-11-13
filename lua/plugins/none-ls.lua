66f753cc36b8ce67d77c657f87f5e6588db77c62
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2024-11-13 13:24:38 +0100

    Just a whole bunch of mypy stuff that only 10% of it worked

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,24 +4,57 @@ return {
 		dependencies = {
 			"nvimtools/none-ls-extras.nvim",
 		},
-		opts = function()
+		opts = function(_, opts)
 			local null_ls = require("null-ls")
+			local virtual = os.getenv("VIRTUAL_ENV")
+				or os.getenv("CONDA_PREFIX")
+				or (vim.fn.finddir(".venv", vim.fn.getcwd() .. ";") and ".venv")
+				or nil
 
-			return {
-				sources = {
-					null_ls.builtins.diagnostics.mypy.with({
-						extra_args = function()
-							local virtual = os.getenv("VIRTUAL_ENV")
-								or os.getenv("CONDA_PREFIX")
-								or (vim.fn.finddir(".venv", vim.fn.getcwd() .. ";") and ".venv")
-								or "/usr"
-							return { "--python-executable", virtual .. "/bin/python3" }
-						end,
-					}),
-				},
-			}
+			opts.sources = vim.list_extend(opts.sources or {}, {
+				null_ls.builtins.diagnostics.mypy.with({
+					extra_args = { "--python-executable", (virtual or "/usr") .. "/bin/python" },
+				}),
+				-- null_ls.builtins.diagnostics.mypy.with({
+				-- 	-- command = virtual and (virtual .. "/bin/dmypy") or "dmypy",
+				-- 	command = "dmypy",
+				-- 	args = function(params)
+				-- 		local t1 = {
+				-- 			"run",
+				-- 			"--timeout",
+				-- 			"5000000",
+				-- 			"--",
+				-- 			"--hide-error-context",
+				-- 			"--no-color-output",
+				-- 			"--show-absolute-path",
+				-- 			"--show-column-numbers",
+				-- 			"--show-error-codes",
+				-- 			"--no-error-summary",
+				-- 			"--no-pretty",
+				-- 			"--cache-fine-grained",
+				-- 			"--sqlite-cache",
+				-- 			"--python-executable",
+				-- 			(virtual or "/usr") .. "/bin/python",
+				--             vim.fn.getcwd()
+				-- 		}
+				-- 		-- local t2 = vim.lsp.buf.list_workspace_folders()
+				-- 		-- for _, v in ipairs(t2) do
+				-- 		-- 	table.insert(t1, v)
+				-- 		-- end
+				-- 		return t1
+				-- 	end,
+				-- 	timeout = 500000000,
+				-- 	-- Do not run in fugitive windows, or when inside of a .venv area
+				-- 	runtime_condition = function(params)
+				-- 		if string.find(params.bufname, "fugitive") or string.find(params.bufname, ".venv") then
+				-- 			return false
+				-- 		else
+				-- 			return true
+				-- 		end
+				-- 	end,
+				-- }),
+			})
 		end,
-
 		init = function()
 			vim.keymap.set("n", "<leader>gf", function()
 				vim.lsp.buf.format({
@@ -46,7 +79,7 @@ return {
 				"stylua",
 				"gitsigns",
 				"gitrebase",
-				"codespell",
+				-- "codespell",
 				"mypy",
 				"rpmspec",
 				"checkmake",
