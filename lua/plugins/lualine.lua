dde5aefd290fa18ab68d1045a55fbbf8d77d5794
Karl Erik Hofseth <karl@kehofseth.no>
2025-02-17 03:09:34 +0100

    Add remote-nvim

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,16 @@ return {
 		},
 		sections = {
 			lualine_b = { { "b:gitsigns_head", icon = "" } },
-			lualine_c = { { "filename", path = 1 } },
+			lualine_c = {
+				{ "filename", path = 1 },
+				{
+					function()
+						return vim.g.remote_neovim_host and ("Remote: %s"):format(vim.uv.os_gethostname()) or ""
+					end,
+					padding = { right = 1, left = 1 },
+					separator = { left = "", right = "" },
+				},
+			},
 		},
 	},
 }
