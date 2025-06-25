d159147d39840aeac54adf986a8eab93c285e23b
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2025-06-25 14:20:31 +0200

    Use a branch that fixes some remote-nvim stuff

--- lua/plugins/remote-nvim.lua
+++ lua/plugins/remote-nvim.lua
@@ -1,5 +1,5 @@
 return {
-	"amitds1997/remote-nvim.nvim",
+    "hmk114/remote-nvim.nvim",
 	version = "*", -- Pin to GitHub releases
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- For standard functions
